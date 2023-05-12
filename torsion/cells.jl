fc004ee25274a4552d2ea6db60fb966ed9f03af2
Simo Nikula <simo.nikula@gmail.com>
2023-05-12 21:24:51 +0300

    better comment

--- torsion/cells.jl
+++ torsion/cells.jl
@@ -2,7 +2,7 @@
 # https://www.julia-vscode.org/docs/dev/userguide/runningcode/
 # The-Julia-REPL
 # @enter println("Test")
-## reload rectangle
+## (re)load rectangle
 include("rectangle.jl")
 ## run rectangle simulation
 Rectangle.main(r=3,n=6,nsteps=1)
