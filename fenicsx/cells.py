1032a3bde2bb57531c8a8d7f8821e0e37799001d
Simo Nikula <simo.nikula@gmail.com>
2023-07-30 22:03:23 +0300

    warping using fenicsx started

--- fenicsx/cells.py
+++ fenicsx/cells.py
@@ -8,3 +8,15 @@ importlib.reload(rectangle)
 timeit.timeit(lambda:rectangle.main(order=1,nx=100,ny=2),number=1)
 # %%
 rectangle.main(order=3)
+# %%
+import importlib
+import warping
+import timeit
+# %%
+import importlib
+importlib.reload(warping)
+# %%
+timeit.timeit(lambda:warping.main(order=1,nx=100,ny=2),number=1)
+# %%
+warping.main(order=2)
+# %%
---
+++ fenicsx/warping.py
@@ -0,0 +1,84 @@
+# Initially based on https://jsdokken.com/dolfinx-tutorial/chapter1/fundamentals_code.html
+# 
+import numpy as np
+import ufl
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+from dolfinx import mesh, fem, plot, io
+def main(order=1,h=0.1,w=0.1,nx=10,ny=10):
+    domain = mesh.create_rectangle(MPI.COMM_WORLD, 
+        [np.array([0.0, 0.0]), np.array([w, h])],
+        [nx,ny], 
+        cell_type=mesh.CellType.quadrilateral)
+    V = fem.VectorFunctionSpace(domain, ("CG", order))
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    F = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx 
+    x = ufl.SpatialCoordinate(domain)
+    n = ufl.FacetNormal(domain)
+    boundaries = [(1, lambda x: np.isclose(x[0], 0)),
+              (2, lambda x: np.isclose(x[0], w)),
+              (3, lambda x: np.isclose(x[1], 0)),
+              (4, lambda x: np.isclose(x[1], h))]
+    facet_indices, facet_markers = [], []
+    fdim = domain.topology.dim - 1
+    for (marker, locator) in boundaries:
+        facets = mesh.locate_entities(domain, fdim, locator)
+        facet_indices.append(facets)
+        facet_markers.append(np.full_like(facets, marker))
+    facet_indices = np.hstack(facet_indices).astype(np.int32)
+    facet_markers = np.hstack(facet_markers).astype(np.int32)
+    sorted_facets = np.argsort(facet_indices)
+    facet_tag = mesh.meshtags(domain, fdim, 
+                              facet_indices[sorted_facets], 
+                              facet_markers[sorted_facets])
+    domain.topology.create_connectivity(domain.topology.dim-1, domain.topology.dim)
+    fn="../paraview/warping_facet_tags.xdmf"
+    with io.XDMFFile(domain.comm, fn, "w") as xdmf:
+        xdmf.write_mesh(domain)
+        xdmf.write_meshtags(facet_tag)
+        print(f"Wrote tags to {fn}.")
+    ds = ufl.Measure("ds", domain=domain, subdomain_data=facet_tag)
+    class BoundaryCondition():
+        def __init__(self, type, marker, values):
+            self._type = type
+            if type == "Dirichlet":
+                u_D = fem.Function(V)
+                u_D.interpolate(values)
+                facets = facet_tag.find(marker)
+                dofs = mesh.locate_dofs_topological(V, fdim, facets)
+                self._bc = fem.dirichletbc(u_D, dofs)
+            elif type == "Neumann":
+                    self._bc = ufl.inner(values, v) * ds(marker)
+            elif type == "Robin":
+                self._bc = values[0] * ufl.inner(u-values[1], v)* ds(marker)
+            else:
+                raise TypeError("Unknown boundary condition: {0:s}".format(type))
+        @property
+        def bc(self):
+            return self._bc
+
+        @property
+        def type(self):
+            return self._type    
+    boundary_conditions = [BoundaryCondition("Robin", 1, n),
+        BoundaryCondition("Robin", 2, n),
+        BoundaryCondition("Robin", 3, n),
+        BoundaryCondition("Robin", 4, n)] 
+    bcs = []
+    for condition in boundary_conditions:
+        if condition.type == "Dirichlet":
+            bcs.append(condition.bc)
+        else:
+            F += condition.bc
+    a = ufl.lhs(F)
+    L = ufl.rhs(F)
+    problem = fem.petsc.LinearProblem(a, L, bcs=bcs, 
+        petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    uh = problem.solve()
+    fn="../paraview/warping.xdmf"
+    with io.XDMFFile(domain.comm, fn, "w") as xdmf:
+        xdmf.write_mesh(domain)
+        uh.name = "Warping"
+        xdmf.write_function(uh)
+    print("Wrote warping to {0}".format(fn))
